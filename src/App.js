236f12d6510ec953bf17e14e9d812e6fc9460265
deepak <deepakajay77@gmail.com>
2022-05-01 10:50:46 +0530

    positioning dropdown displaying all countries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-
-import './App.css';
+import "./App.css";
 
 import React, { useState, useEffect } from "react";
 import "./App.css";
@@ -54,8 +53,26 @@ function App() {
       });
   };
   return (
-    <div className="App">
-     <h1>Welcome to react</h1>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats"></div>
+      </div>
+      <Card className="app__right"></Card>
     </div>
   );
 }
