901c76b261cc18b79ae4e227cd13d66f818b7a49
deepak <deepakajay77@gmail.com>
2022-05-01 14:40:53 +0530

    Updates

---
+++ precache-manifest.433415de0fefdc351bdcb808895bc25f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16ee26f1519e43e177f40f3b839c138",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f4be9e9d45d0f0979e7a",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d2980e426930f6c5894a",
+    "url": "/covid-tracker/static/css/main.9459914e.chunk.css"
+  },
+  {
+    "revision": "f4be9e9d45d0f0979e7a",
+    "url": "/covid-tracker/static/js/2.f8936bde.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.f8936bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2980e426930f6c5894a",
+    "url": "/covid-tracker/static/js/main.da4795c2.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
